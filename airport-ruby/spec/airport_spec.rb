8e95ad837dea5081f1ad539f5e59cebfe251f242
Reenakoudi <kreena92@gmail.com>
2018-04-22 15:06:48 +0100

    Updated take_off to confirm plane no longer exists at airport after take_off

--- airport-ruby/spec/airport_spec.rb
+++ airport-ruby/spec/airport_spec.rb
@@ -2,18 +2,25 @@ require 'airport'
 require 'plane'
 
 describe Airport do
-  let(:airport) { Airport.new }
-  let(:plane) { Plane.new }
+  subject(:airport) { Airport.new }
+  let(:plane1) { Plane.new(700) }
+  let(:plane2) { Plane.new(400) }
+
   describe '#land'do
     it 'instructs the plane to land' do
-      expect(airport.land(plane)).to eq(plane)
+      expect(airport.land(plane1)).to eq([plane1])
     end
   end
 
   describe '#take_off' do
-    it 'instructs the plane to take-off' do
-      airport.land(plane)
-      expect(airport.take_off(plane)).not_to eq(plane)
+    it 'instructs one plane to land and take_off' do
+      airport.land(plane1)
+      expect(airport.take_off(plane1)).to eq([])
+    end
+    it 'it confirms the plane has taken off and no longer in airport' do
+      airport.land(plane1)
+      airport.land(plane2)
+      expect(airport.take_off(plane1)).to eq([plane2])
     end
   end
 end
