02639a1b4898d51e4c73375893e69c5a4b8de148
Reenakoudi <kreena92@gmail.com>
2018-04-22 12:43:20 +0100

    Created Plane class and added land method to Airport

--- airport-ruby/lib/airport.rb
+++ airport-ruby/lib/airport.rb
@@ -1,2 +1,7 @@
+require_relative 'plane'
+
 class Airport
+  def land(plane)
+    plane
+  end
 end
--- airport-ruby/spec/airport_spec.rb
+++ airport-ruby/spec/airport_spec.rb
@@ -1,7 +1,9 @@
 require 'airport'
+require 'plane'
 
 describe Airport do
   let(:airport) { Airport.new }
+  let(:plane) { Plane.new }
   describe '#land'do
     it 'instructs the plane to land' do
       expect(airport.land(plane)).to eq(plane)
