c6292c80ff441131c8b7f2401dc53a0d4da8640a
Reenakoudi <kreena92@gmail.com>
2018-04-22 18:59:45 +0100

    Refactoring based on context weather is sunny or stormy

--- airport-ruby/spec/airport_spec.rb
+++ airport-ruby/spec/airport_spec.rb
@@ -7,38 +7,50 @@ describe Airport do
   let(:plane2) { double("plane2") }
 
 
-  describe '#land'do
-    it 'instructs the plane to land' do
+  describe 'When weather is sunny' do
+    before do
       allow(airport.weather).to receive(:stormy?).and_return(false)
-      expect(airport.land(plane1)).to eq([plane1])
     end
 
-    it 'Prevents the plane from landing if stormy' do
-      allow(airport.weather).to receive(:stormy?).and_return(true)
-      expect{ airport.land(plane1) }.to raise_error 'Landing cancelled due to bad weather'
+    context '#land' do
+      it 'instructs the plane to land' do
+        expect(airport.land(plane1)).to eq([plane1])
+      end
     end
+
+    context '#take_off' do
+      it 'instructs one plane to land and take_off' do
+        airport.land(plane1)
+        expect(airport.take_off(plane1)).to eq([])
+      end
+
+      it 'it confirms the plane has taken off and no longer in airport' do
+        airport.land(plane1)
+        airport.land(plane2)
+        expect(airport.take_off(plane1)).to eq([plane2])
+      end
+    end
+
   end
 
-  describe '#take_off' do
-    it 'instructs one plane to land and take_off' do
-      allow(airport.weather).to receive(:stormy?).and_return(false)
-      airport.land(plane1)
-      allow(airport.weather).to receive(:stormy?).and_return(false)
-      expect(airport.take_off(plane1)).to eq([])
-    end
-    it 'it confirms the plane has taken off and no longer in airport' do
-      allow(airport.weather).to receive(:stormy?).and_return(false)
-      airport.land(plane1)
-      airport.land(plane2)
-      allow(airport.weather).to receive(:stormy?).and_return(false)
-      expect(airport.take_off(plane1)).to eq([plane2])
-    end
+    describe 'When weather is stormy' do
+      before do
+        allow(airport.weather).to receive(:stormy?).and_return(true)
+      end
 
-    it 'Prevent the plane from take_off if stormy' do
-      allow(airport.weather).to receive(:stormy?).and_return(false)
-      airport.land(plane1)
-      allow(airport.weather).to receive(:stormy?).and_return(true)
-      expect{ airport.take_off(plane1) }.to raise_error 'Take_off cancelled due to bad weather'
-    end
-  end
-end
+      context '#land' do
+        it 'Prevents the plane from landing if stormy' do
+          expect{ airport.land(plane1) }.to raise_error 'Landing cancelled due to bad weather'
+        end
+      end
+
+     context '#take_off' do
+       it 'Prevent the plane from take_off if stormy' do
+         allow(airport.weather).to receive(:stormy?).and_return(false)
+         airport.land(plane1)
+         allow(airport.weather).to receive(:stormy?).and_return(true)
+         expect{ airport.take_off(plane1) }.to raise_error 'Take_off cancelled due to bad weather'
+       end
+     end
+   end
+ end
